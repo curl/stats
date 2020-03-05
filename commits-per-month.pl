1279b0c784a923e134c39a847fa905f57d63e6fa
Daniel Stenberg <daniel@haxx.se>
2020-03-05 22:49:39 +0100

    commits-per-month.pl

---
+++ commits-per-month.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+
+open(G, "git log --reverse --pretty=fuller --no-color --date=short --decorate=full|");
+
+while(<G>) {
+    chomp;
+    $line = $_;
+    if($line =~ /^CommitDate:([ \t]*)(.*)/) {
+        $date = $2;
+        if($date =~ /^(\d\d\d\d)-(\d\d)/) {
+            $period = "$1-$2";
+        }
+        $commit{$period}++;
+    }
+}
+
+for my $y (sort keys %commit) {
+    printf "%s-01;%d\n", $y, $commit{$y};
+}
