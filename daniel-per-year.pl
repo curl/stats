a6237292e455f179e887447530a01131196e9f97
Daniel Stenberg <daniel@haxx.se>
2020-03-05 23:08:55 +0100

    daniel-per-year: daniel's share of commits per year

---
+++ daniel-per-year.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+my @a = `git log --use-mailmap --reverse --pretty=fuller --no-color --date=short --decorate=full | egrep "^(Author|CommitDate):"`;
+
+my $c=1;
+my $yearmon;
+my $auth;
+for(@a) {
+    chomp;
+    my $line = $_;
+    if(/^Author: *([^\<]*) \</) {
+        ($auth)=($1);
+    }
+    elsif(/^CommitDate: (.*)/) {
+        if($1 =~ /^(\d\d\d\d)-(\d\d)/) {
+            $yearmon = "$1";
+        }
+        $who{$auth}++;
+        $commits{$auth.$yearmon}++;
+        $when{$yearmon}++;
+        $c++;
+    }
+}
+
+# Daniel's percentage share per year
+
+for my $y(sort keys %when) {
+    printf("%s-12-31;%.1f\n", $y, $commits{"Daniel Stenberg".$y}*100/$when{$y} );
+}
