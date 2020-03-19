91971c08963e1dfecb2131c1eb58ec7c23acdce0
Daniel Stenberg <daniel@haxx.se>
2020-03-19 22:44:01 +0100

    lines-over-time: remove the hash to version lookup

--- lines-over-time.pl
+++ lines-over-time.pl
@@ -1,44 +1,26 @@
 #!/usr/bin/perl
 
-#open(L, "git log --reverse --date=short --stat -- ':!docs' |");
 open(L, "git log --reverse --pretty=fuller --date=short --stat -- src lib include |");
-#open(L, "git log --reverse --pretty=fuller --date=short --stat -- ':!docs' |");
-#open(L, "git log --reverse --pretty=fuller --date=short --stat -- tests |");
-
-# map commit hashes to release tags
-sub commit2release {
-    open(T, "git for-each-ref refs/tags/*|");
-    # 329bcf3a7117c7e5c26d7c8f840af64fb7140753 commit refs/tags/curl-7_9_1
-    while(<T>) {
- #       print "IN $_";
-        if($_ =~ /^([0-9a-f]+) (commit|tag)[ \t]+refs\/tags\/(curl-[0-9_]*)$/) {
-            my ($hash, $type, $version)=($1, $2, $3);
-
-            # if it is a tag, we must find the commit
-            my $actual = `git rev-list -n 1 $hash`;
-            chomp $actual;
-            if($actual ne $hash) {
-                print "$hash $type => $actual\n";
-                $hash = $actual;
-            }
-
-            # convert to plain version number
-            $version =~ s/curl-//;
-            $version =~ s/_/./g;
-            $commit{$hash} = $version;
-#            print "$hash is $version\n";
-        }
-    }
-    close(T);
-}
-
-#commit2release();
 
 my $date;
 my $lines;
 my $c;
-my $release;
 my $hash;
+
+# the inital amount is an estimate
+# curl 4.8 is 1998-09-20
+# curl 5.9 is 1999-05-22
+# curl 6.0 is 1999-09-13
+# curl 6.3.1 is 1999-11-23
+print <<EARLY
+1998-03-18;2200
+1998-09-20;3379
+1999-05-22;12289
+1999-09-13;14060
+1999-11-23;14826
+EARLY
+    ;
+
 while(<L>) {
     chomp;
     my $line = $_;
@@ -75,21 +57,8 @@ while(<L>) {
 
         # new date
         if($sdate ne $ldate) {
-            my $contains = `git describe --contains $hash --exact-match --match "curl*" --exclude '*[^0-9]' 2>/dev/null`;
-            chomp $contains;
-            if($contains =~ /curl-([0-9_]*)(~|$)/) {
-                $version = $1;
-                $version =~ s/curl-//;
-                $version =~ s/_/./g;
-                $release = $version;
-            }
-            else {
-                $release ="";
-            }
-
-            print "$date;$lines;$release\n";
+            print "$date;$lines\n";
             $ldate = $sdate;
-            $lrelease = $release
         }
         $c++;
         $date="";
