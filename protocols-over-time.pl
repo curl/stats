9ba95e31fbd2b61352ccc1084ca0130d02378386
Daniel Stenberg <daniel@haxx.se>
2020-03-20 11:17:48 +0100

    protocols-over-time: yes

---
+++ protocols-over-time.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+open(T, "<stats/protocol-history.md");
+while(<T>) {
+    chomp;
+    if($_ =~ /^([^ ]*) (\d\d\d\d-\d\d-\d\d)/) {
+        $c++;
+        print "$2;$1;$c\n";
+        $end = $c;
+    }
+}
+close(T);
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+    localtime(time);
+printf "%04d-%02d-%02d;;%d\n", $year + 1900, $mon + 1, $mday, $end;
