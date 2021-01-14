a5c2f50b320fcdff4bcb9c082ec9fe3752648609
Daniel Stenberg <daniel@haxx.se>
2021-01-14 11:23:58 +0100

    daniel-vs-rest: add graphs for added and removed lines

--- daniel-vs-rest.pl
+++ daniel-vs-rest.pl
@@ -2,13 +2,13 @@
 
 my $c=0;
 
-open(G, "git log --reverse --pretty=fuller --no-color --date=short --decorate=full|");
+open(G, "git log --reverse --pretty=fuller --no-color --date=short --decorate=full --stat|");
 
 while(<G>) {
     chomp;
     my $line = $_;
     if($line =~ /^CommitDate: (\d\d\d\d-\d\d)/) {
-        my $date = $1; # year + month
+        $date = $1; # year + month
         $commit{$author,$date}++;
         $total{$author}++;
         $when{$date}++;
@@ -21,14 +21,45 @@ while(<G>) {
         }
         $uniq{$author}++;
     }
+    elsif($line =~ /^ (\d*) file(|s) changed, (.*)/) {
+        my $d=$3;
+        my $adds=0;
+        my $dels=0;
+        $d =~ s/, //; #remove comma
+        if($d =~ s/(\d*) insertion(s|)\(\+\)//) {
+            $adds = $1;
+        }
+        if($d =~ s/(\d*) deletion(s|)\(\-\)//) {
+            $dels = $1;
+        }
+
+        #print STDERR "$date $author += $lines\n";
+        $add{$author, $date} += $adds;
+        $del{$author, $date} += $dels;
+    }
 }
 close(G);
 
 for my $date (sort keys %when) {
+    #print STDERR "$date: $when{$date}\n";
     $alld += $commit{"Daniel Stenberg", $date};
     $alltot += $when{$date};
-    printf "$date-01;%.2f;%.2f;%.2f\n",
+
+    $dadd += $add{"Daniel Stenberg", $date};
+    $alladd += $add{"Daniel Stenberg", $date} +
+        $add{"else", $date};
+    my $addshare = 100;
+    $addshare = $dadd / $alladd * 100 if($alladd);
+
+    $ddel += $del{"Daniel Stenberg", $date};
+    $alldel += $del{"Daniel Stenberg", $date} +
+        $del{"else", $date};
+    my $delshare = 100;
+    $delshare = $ddel / $alldel * 100 if($alldel);
+
+    printf "$date-01;%.2f;%.2f;%.2f;%.2f;%.2f\n",
         ($alld/$alltot * 100),
         100 - ($alld/$alltot * 100),
-        $commit{"Daniel Stenberg", $date}/$when{$date} * 100;
+        $commit{"Daniel Stenberg", $date}/$when{$date} * 100,
+        $addshare, $delshare;
 }
--- daniel-vs-rest.plot
+++ daniel-vs-rest.plot
@@ -2,23 +2,26 @@
 set terminal svg size 1920,1080 dynamic font ",24"
 
 # title
-set title "Daniel's share of commits" font ",48"
+set title "Daniel Stenberg's share of commits" font ",48"
 # where's the legend
 set key top right
 
 # Identify the axes
 #set xlabel "Time"
-set ylabel "Percentage of git commits"
+set ylabel "Percentage"
 
 set style line 1 \
-    linecolor rgb '#0060ad' \
-    linetype 1 linewidth 5
-set style line 2 \
     linecolor rgb '#40c040' \
     linetype 1 linewidth 5
+set style line 2 \
+    linecolor rgb '#0060ad' \
+    linetype 1 linewidth 2
 set style line 3 \
     linecolor rgb '#a0a0a0' \
     linetype 3 linewidth 1
+set style line 4 \
+    linecolor rgb '#c04040' \
+    linetype 3 linewidth 2
 
 set grid
 unset border
@@ -31,7 +34,7 @@ set ytics 10
 set yrange [0:]
 set xrange ["1999-10-01":]
 
-set boxwidth 0.2 relative
+set boxwidth 0.5 relative
 set style fill solid
 
 # set the format of the dates on the x axis
@@ -39,7 +42,7 @@ set format x "%Y"
 set xtics rotate 3600*24*365.25
 
 set datafile separator ";"
-plot  'tmp/daniel-vs-rest.csv' using 1:4 with boxes linestyle 3 title "Daniel's monthly share", \
-'tmp/daniel-vs-rest.csv' using 1:2 with lines linestyle 1 title "Daniel Stenberg accumulated", \
- 'tmp/daniel-vs-rest.csv' using 1:3 with lines linestyle 2 title "Everyone else accumulated"
- 
+plot  'tmp/daniel-vs-rest.csv' using 1:4 with boxes linestyle 3 title "Monthly share of commit count", \
+'tmp/daniel-vs-rest.csv' using 1:2 with lines linestyle 1 title "share of commit count", \
+'tmp/daniel-vs-rest.csv' using 1:5 with lines linestyle 4 title "share of added lines", \
+'tmp/daniel-vs-rest.csv' using 1:6 with lines linestyle 2 title "share of removed lines"
