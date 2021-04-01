385ed43ea58d17539f999e9d9766aab070901d5c
Daniel Stenberg <daniel@haxx.se>
2021-04-01 09:58:29 +0200

    vulns-per-year: figure out and use the current year as end

--- vulns-per-year.pl
+++ vulns-per-year.pl
@@ -12,7 +12,12 @@ for(@vuln) {
     $v{$year}++;
 }
 
-for(1998 .. 2020) {
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+    localtime(time);
+$year += 1900;
+
+# go to this year
+for(1998 .. $year) {
     $total += $v{$_};
     # writes a full date just to be parsable
     printf "%d-01-01;%d;%d\n", $_, $v{$_}, $total;
