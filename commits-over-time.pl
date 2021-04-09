a561d72c5aeb8ee999d0714bada9e2869190b513
Daniel Stenberg <daniel@haxx.se>
2021-04-09 23:50:43 +0200

    commits-over-time: add "Daniel" and "everyone" plots too

--- commits-over-time.pl
+++ commits-over-time.pl
@@ -3,21 +3,33 @@
 
 open(G, "git log --reverse --pretty=fuller --no-color --date=short --decorate=full|");
 
+my $author;
 while(<G>) {
     chomp;
     $line = $_;
-    if($line =~ /^CommitDate:([ \t]*)(.*)/) {
+    if($line =~ /^Author: *([^\<]*) \</) {
+        $author=$1;
+        if($author ne "Daniel Stenberg") {
+            $author = "else";
+        }
+    }
+    elsif($line =~ /^CommitDate:([ \t]*)(.*)/) {
         $date = $2;
         if($date =~ /^(\d\d\d\d)-(\d\d)/) {
             $period = "$1-$2";
         }
         $commit{$period}++;
+        $commituser{$period.$author}++;
     }
 }
 
 my @pp;
 my $total;
+my $d;
+my $r;
 for my $y (sort keys %commit) {
     $total += $commit{$y};
-    printf "%s-01;%d\n", $y, $total;
+    $d += $commituser{$y."Daniel Stenberg"};
+    $r += $commituser{$y."else"};
+    printf "%s-01;%u;%u;%u\n", $y, $total, $d, $r;
 }
--- commits-over-time.plot
+++ commits-over-time.plot
@@ -12,7 +12,13 @@ set ylabel "Number of commits"
 
 set style line 1 \
     linecolor rgb '#40c0ad' \
-    linetype 1 linewidth 4
+    linetype 1 linewidth 2
+set style line 2 \
+    linecolor rgb '#c04040' \
+    linewidth 2
+set style line 3 \
+    linecolor rgb '#4040c0' \
+    linewidth 2
 
 set grid
 unset border
@@ -25,4 +31,6 @@ set xtics rotate 3600*24*365.25 nomirror
 # set the format of the dates on the x axis
 set format x "%Y"
 set datafile separator ";"
-plot 'tmp/commits-over-time.csv' using 1:2 with lines linestyle 1 title ""
+plot'tmp/commits-over-time.csv' using 1:3 with lines linestyle 2 title "Daniel's commits", \
+  'tmp/commits-over-time.csv' using 1:4 with lines linestyle 3 title "Everyone else's commits", \
+  'tmp/commits-over-time.csv' using 1:2 with lines linestyle 1 title "All commits"
