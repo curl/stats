c5b4f8483379c7053e6e8a15b5c0f74cec23eb5f
Daniel Stenberg <daniel@haxx.se>
2024-02-09 12:30:31 +0100

    contrib-tail.pl: rewrite to avoid shortlog

--- contrib-tail.pl
+++ contrib-tail.pl
@@ -1,20 +1,20 @@
 #!/usr/bin/perl
 
-my $c="LANG=C git shortlog -s";
-my @all=`$c`;
+# shortlog does not work like we want, make our own
+my $c="git log";
 
-for $e (@all) {
-    if($e =~ /^ *(\d+)[ \t]+(.*)/) {
-        $commits{$2} = $1;
+open(G, "$c|");
+
+while(<G>) {
+    my $e = $_;
+    chomp $e;
+    if($e =~ /^Author: (.*) </) {
+        $commits{$1}++;
     }
 }
+close(G);
 
 my $index = 1;
 for $a (sort {$commits{$b} <=> $commits{$a}} keys %commits) {
     printf "%s;%u;%u\n", $a, $commits{$a}, $index++;
-}
-if($index == 1) {
-    print "badness ensued: \n";
-    print `pwd`;
-    print `$c 2>&1`;
 }
