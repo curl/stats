d4e9408f405350188f09475cbc2a263d5736df93
Daniel Stenberg <daniel@haxx.se>
2025-01-04 23:49:22 +0100

    addedcode: just one output line per day

--- addedcode.pl
+++ addedcode.pl
@@ -3,6 +3,16 @@
 open(G, "git log --reverse --stat --date=short -- src lib include 2>/dev/null|");
 my $date;
 my $lines;
+my $pdate;
+
+sub display {
+    my ($d)=@_;
+    if($d ne $pdate) {
+        printf "$pdate;$lines;%d\n", $l{$pdate};
+        $pdate = $d;
+    }
+}
+
 while(<G>) {
     if(/^Date: *(\d\d\d\d-\d\d-\d\d)/) {
         $date = $1;
@@ -11,8 +21,9 @@ while(<G>) {
         if(!$date) {
             die "bad: $1";
         }
+        $l{$date} += $1;
+        display($date);
         $lines += $1;
-        print "$date;$lines;$1\n";
         undef $date;
     }
     chomp;
