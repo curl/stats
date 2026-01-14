bc33bdec20956b813038a987725ab9b0d8c94902
Daniel Stenberg <daniel@haxx.se>
2026-01-14 14:00:51 +0100

    line-complex.pl: remove the local median function

--- line-complex.pl
+++ line-complex.pl
@@ -26,19 +26,6 @@ foreach my $t (@alltags) {
     }
 }
 
-sub median {
-    my @a = @_;
-    my @vals = sort {$a <=> $b} @a;
-    my $len = @vals;
-    if($len%2) { #odd?
-        return $vals[int($len/2)];
-    }
-    else {
-        #even
-        return ($vals[int($len/2)-1] + $vals[int($len/2)])/2;
-    }
-}
-
 sub p99 {
     my @a = @_;
     my @vals = sort {$b <=> $a} @a;
