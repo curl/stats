3b932af358d4cebda3c3184bcde122c2a5a4b43a
Daniel Stenberg <daniel@haxx.se>
2026-07-01 14:50:30 +0200

    authors-percent: number of authors did X% of all commits
    70, 80 and 90 of all commits were done by this many authors, over time
    
    Closes #45

---
+++ authors-percent.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+require "./stats/average.pm";
+
+my @a = `git log --use-mailmap --reverse --pretty=fuller --no-color --date=short --decorate=full  | grep -E "^(Author|CommitDate):"`;
+
+my $percent = $ARGV[0] || 90;
+
+sub amount {
+    my $share = 0;
+    my $count = 0;
+    for my $a (sort {$uniq{$b} <=> $uniq{$a}} keys %uniq) {
+        $share += $uniq{$a} * 100 / $total;
+        $count++;
+        last if($share >= $percent);
+    }
+    return $count;
+}
+
+my $pa;
+my $date;
+for(@a) {
+    chomp;
+    my $line = $_;
+    if(/^CommitDate: (\d\d\d\d-\d\d-\d\d)/) {
+        $date = $1;
+        my $a = amount();
+        if($a != $pa) {
+            printf "$date;%u\n", $a;
+            $pa = $a;
+        }
+    }
+    if(/^Author: *([^\<]*) \</) {
+        my ($auth)=($1);
+        $total++;
+        $uniq{$auth}++;
+    }
+}
+print "$date;$pa\n";
